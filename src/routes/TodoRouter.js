7c576224c29f8d3d81ba789722e48385b5dd0b32
YoProgrammer <yuttasakcom@gmail.com>
2017-12-03 14:07:46 +0700

    create api delete /todos/{id} and get /todos/{id}

--- src/routes/TodoRouter.js
+++ src/routes/TodoRouter.js
@@ -4,8 +4,8 @@ const TodoController = require('../controllers/TodoController')
 
 Router.get('/', TodoController.get)
 Router.post('/create', TodoController.create)
-Router.patch('/:id/edit', TodoController.update)
-Router.get('/:id/delete', TodoController.destroy)
+Router.put('/:id', TodoController.update)
+Router.delete('/:id', TodoController.destroy)
 Router.get('/:id', TodoController.getById)
 
 module.exports = Router
--- src/services/files/get.js
+++ src/services/files/get.js
@@ -1,14 +1,5 @@
 const fs = require('fs')
 
-// Callback
-// module.exports = (file, callback) => {
-//   fs.readFile(file, 'utf-8', (err, data) => {
-//     if (err) callback(err, null)
-//     callback(null, data)
-//   })
-// }
-
-// Promise
 module.exports = file => {
   return new Promise((resolve, reject) => fs.readFile(file, 'utf-8', (err, data) => {
     if (err) reject(err)
