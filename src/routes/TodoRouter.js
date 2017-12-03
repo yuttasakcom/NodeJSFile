3433cb550c08245bf7e6beae59773f99942cde39
YoProgrammer <yuttasakcom@gmail.com>
2017-12-03 13:56:34 +0700

    create api /todos/:{id}/edit finish

--- src/routes/TodoRouter.js
+++ src/routes/TodoRouter.js
@@ -4,7 +4,7 @@ const TodoController = require('../controllers/TodoController')
 
 Router.get('/', TodoController.get)
 Router.post('/create', TodoController.create)
-Router.get('/:id/edit', TodoController.update)
+Router.patch('/:id/edit', TodoController.update)
 Router.get('/:id/delete', TodoController.destroy)
 Router.get('/:id', TodoController.getById)
 
--- src/services/files/update.js
+++ src/services/files/update.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const moment = require('moment')
 const time = moment()
 
-module.exports = (file, req, callback) => {
+module.exports = (file, {params, body}, callback) => {
   fs.readFile(file, 'utf-8', (err, data) => {
     if (err) callback(err, null)
 
     const currentData = JSON.parse(data)
-    const currentPost = currentData.find(e => e.id === req.params.id)
+    const currentPost = currentData.find(e => e.id === params.id)
 
-    currentPost.topic = 'edit topic'
-    currentPost.content = 'edit content'
+    currentPost.topic = body.topic
+    currentPost.content = body.content
     currentPost.updated_at = time.format()
 
-    fs.writeFile(file, JSON.stringify([...currentData.filter(e => e.id !== req.params.id), currentPost]), (err) => {
+    fs.writeFile(file, JSON.stringify([...currentData.filter(e => e.id !== params.id), currentPost]), (err) => {
       if (err) callback(err, null)
       callback(null, currentPost)
     })
