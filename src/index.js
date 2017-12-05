80b4074b781b972aa3f6cc0ad548b9ea64327205
YoProgrammer <yuttasakcom@gmail.com>
2017-12-05 12:44:04 +0700

    setup cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
@@ -10,5 +12,5 @@ router(app)
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Server running at port:${port}`)
+  console.log(`Server running at localhost:${port}`)
 })
